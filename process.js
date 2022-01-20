10617a477e175fd794a17aa0a485bf702f17cfc5
samuel <samuelcontatodev@gmail.com>
2022-01-20 10:24:13 -0300

    inciando de maneira pratica

--- process.js
+++ process.js
@@ -1,3 +1,6 @@
-const firstName = process.argv[2]
-const lastName = process.argv[3]
-console.log(`Meu nome é ${firstName} ${lastName}`)
+// const firstName = process.argv[2]
+// const lastName = process.argv[3]
+// console.log(`Meu nome é ${firstName} ${lastName}`)
+
+// USANDO FLAGS no terminal: node process --name 'Samuel' --greeting 'Tudo bem?'
+console.log(process.argv)
